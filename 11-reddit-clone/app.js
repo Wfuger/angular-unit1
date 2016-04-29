baa01e95f70405b8039c271f8fa86479acd548f4
Wfuger <wfuger@gmail.com>
2016-04-28 23:32:25 -0600

    angular stop being so awesome

--- 11-reddit-clone/app.js
+++ 11-reddit-clone/app.js
@@ -1,13 +1,17 @@
 angular.module('RedditClone', [])
   .controller('mainController', ['$scope', function($scope){
     $scope.posts = [];
-    $scope.sortPosts = {};
     $scope.view = {};
     $scope.newPost = {};
     $scope.view.createPost = false;
-    $scope.newPost = function() { $scope.view.createPost = !$scope.view.createPost; }
+    $scope.view.by = 'votes';
+    $scope.newPost = function() { return $scope.view.createPost = !$scope.view.createPost; }
     $scope.post = function() {
       $scope.posts.push($scope.newPost);
-      $scope.newPost = {}
+      return $scope.newPost = {};
+      // return $scope.view.createPost = !$scope.view.createPost;
+    }
+    $scope.sorter = function(b) {
+      return $scope.view.by = b;
     }
   }])
