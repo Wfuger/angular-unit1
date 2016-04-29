d6aa1172e66bce4f281b94aaf44af29a5e0fae44
Wfuger <wfuger@gmail.com>
2016-04-28 22:46:25 -0600

    posts are populating

--- 11-reddit-clone/app.js
+++ 11-reddit-clone/app.js
@@ -2,4 +2,12 @@ angular.module('RedditClone', [])
   .controller('mainController', ['$scope', function($scope){
     $scope.posts = [];
     $scope.sortPosts = {};
+    $scope.view = {};
+    $scope.newPost = {};
+    $scope.view.createPost = false;
+    $scope.newPost = function() { $scope.view.createPost = !$scope.view.createPost; }
+    $scope.post = function() {
+      $scope.posts.push($scope.newPost);
+      $scope.newPost = {}
+    }
   }])
