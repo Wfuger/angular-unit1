2be141b957240b25448c101045ec04c5ddf27058
Wfuger <wfuger@gmail.com>
2016-04-30 19:56:20 -0600

    modal for form looks pretty cool

--- 11-reddit-clone/app.js
+++ 11-reddit-clone/app.js
@@ -1,21 +1,56 @@
+var moment = require('moment');
 angular.module('RedditClone', [])
   .controller('mainController', ['$scope', function($scope){
-    $scope.posts = [];
     $scope.view = {};
     $scope.newPost = {};
     $scope.view.createPost = false;
     $scope.view.by = 'votes';
-    $scope.newPost = function() { return $scope.view.createPost = !$scope.view.createPost; }
+    $scope.showPost = function() { $scope.view.createPost = !$scope.view.createPost; }
     $scope.post = function() {
-      $scope.posts.push($scope.newPost);
       $scope.newPost.votes = 0;
-      return $scope.newPost = {};
-      // return $scope.view.createPost = !$scope.view.createPost;
+      $scope.newPost.date = moment().calendar();
+      $scope.newPost.comments = ['poop'];
+      $scope.newPost.showTheComments = false;
+      $scope.posts.push($scope.newPost);
+      $scope.newPost = {};
     }
     $scope.sorter = function(b) {
+      if(b === 'votes') $scope.view.reverse = true;
       return $scope.view.by = b;
     }
-    $scope.vote = function(vote, i) {
-      return vote === 'up' ? $scope.posts[i].votes++ : $scope.posts[i].votes--;
+    $scope.vote = function(vote, post) {
+      return vote === 'up' ? post.votes++ : post.votes--;
+    }
+    $scope.posts = [
+      {
+        title: 'Unique New York',
+        votes: 0,
+        author: 'Yo momma',
+        image: 'http://www.fillmurray.com/200/300',
+        description: '',
+        date: moment().calendar(),
+        comments: []
+      },
+      {
+        title: 'Bill Fucking Murray',
+        votes: 10,
+        author: 'That One Guy',
+        image: 'http://www.fillmurray.com/200/302',
+        description: 'The coolest man alive',
+        date: moment().calendar(),
+        comments: []
+      },
+      {
+        title: 'More Bill Fucking Murray',
+        votes: 5,
+        author: 'Some Dude',
+        image: 'http://www.fillmurray.com/200/303',
+        description: 'The coolest mother fucking man alive',
+        date: moment().calendar(),
+        comments: []
+      }
+    ]
+    $scope.showComments = function(post) {
+      post.showTheComments = !post.showTheComments;
     }
   }])
